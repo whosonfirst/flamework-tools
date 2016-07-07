143d823f213b62155245c5cefe68dd0bdefab5dd
thisisaaroland <aaron@mapzen.com>
2016-07-07 08:35:40 -0700

    tweak setup-db to reflect reality

--- ubuntu/setup-db.sh
+++ ubuntu/setup-db.sh
@@ -44,8 +44,8 @@ fi
 
 echo "DROP DATABASE IF EXISTS ${DBNAME};" >> /tmp/${DBNAME}.sql;
 echo "CREATE DATABASE ${DBNAME};" >> /tmp/${DBNAME}.sql
-# this doesn't work on older versions of mysql but I don't remember which version that is... (20160608/thisisaaronland)
-echo "DROP user '${USERNAME}'@'localhost';" >> /tmp/${DBNAME}.sql
+# this requires mysql 5.7 or higher...
+# echo "DROP USER IF EXISTS '${USERNAME}'@'localhost';" >> /tmp/${DBNAME}.sql
 echo "CREATE user '${USERNAME}'@'localhost' IDENTIFIED BY '${PASSWORD}';" >> /tmp/${DBNAME}.sql
 echo "GRANT SELECT,UPDATE,DELETE,INSERT ON ${DBNAME}.* TO '${USERNAME}'@'localhost' IDENTIFIED BY '${PASSWORD}';" >> /tmp/${DBNAME}.sql
 echo "FLUSH PRIVILEGES;" >> /tmp/${DBNAME}.sql
