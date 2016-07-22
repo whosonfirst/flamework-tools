e24a0e88a6eec94921f4e0f10e4427be39bd9b0b
thisisaaronland <whosonfirst@mapzen.com>
2016-07-22 01:04:56 +0000

    update to reflect php7-isms

--- ubuntu/setup-flamework.sh
+++ ubuntu/setup-flamework.sh
@@ -8,8 +8,8 @@ ROOT=`dirname $UBUNTU`
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install apache2 apache2-utils mysql-server memcache
-sudo apt-get -y install php5 php5-cli php5-curl php5-mcrypt php5-memcache php5-mysql
+sudo apt-get -y install apache2 apache2-utils mysql-server memcached
+sudo apt-get -y install libapache2-mod-php php php-cli php-curl php-mcrypt php-memcache php-mysql php-mbstring
 
 for mod in proxy_wstunnel.load rewrite.load proxy.load proxy.conf proxy_http.load ssl.conf ssl.load socache_shmcb.load headers.load
 do
@@ -43,23 +43,23 @@ done
 for ctx in apache2 cli
 do
 
-    for mod in mcrypt.ini
-    do
+#    for mod in mcrypt.ini
+#    do
+#
+#        if [ -L /etc/php/7.0/${ctx}/conf.d/${mod} ]
+#        then
+#            sudo rm /etc/7.0/php/${ctx}/conf.d/${mod}
+#        fi
+#
+#        if [ -f /etc/php/7.0/${ctx}/conf.d/${mod} ]
+#        then
+#            sudo mv /etc/php/7.0/${ctx}/conf.d/${mod} /etc/php/7.0/${ctx}/conf.d/${mod}.bak
+#        fi
+#
+#        sudo ln -s /etc/php/7.0/mods-available/${mod} /etc/php/7.0/${ctx}/conf.d/${mod}
+#    done
 
-        if [ -L /etc/php5/${ctx}/conf.d/${mod} ]
-        then
-            sudo rm /etc/php5/${ctx}/conf.d/${mod}
-        fi
-
-        if [ -f /etc/php5/${ctx}/conf.d/${mod} ]
-        then
-            sudo mv /etc/php5/${ctx}/conf.d/${mod} /etc/php5/${ctx}/conf.d/${mod}.bak
-        fi
-
-        sudo ln -s /etc/php5/mods-available/${mod} /etc/php5/${ctx}/conf.d/${mod}
-    done
-
-    sudo perl -p -i -e "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/${ctx}/php.ini;
+    sudo perl -p -i -e "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.0/${ctx}/php.ini;
 done
 
 if [ ! -d ${ROOT}/www/templates_c ]
