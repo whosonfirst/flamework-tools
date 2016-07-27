a58eacd874fb97ab839acdcbdf2534b357696403
thisisaaronland <whosonfirst@mapzen.com>
2016-07-27 18:25:43 +0000

    support for flamework accounts branch

--- ubuntu/setup-ubuntu.sh
+++ ubuntu/setup-ubuntu.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 sudo apt-get install -y git tcsh emacs24-nox htop sysstat ufw fail2ban unattended-upgrades python-setuptools unzip
-sudo dpkg-reconfigure --priority=low unattended-upgrades
+sudo dpkg-reconfigure -f noninteractive --priority=low unattended-upgrades
 
 exit 0
