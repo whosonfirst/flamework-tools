82c8569e5848ecddce388225f1c182795e71fd05
thisisaaroland <aaron@mapzen.com>
2016-09-06 15:08:30 -0700

    check for 14.04

--- ubuntu/setup-flamework.sh
+++ ubuntu/setup-flamework.sh
@@ -8,8 +8,16 @@ ROOT=`dirname $UBUNTU`
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install apache2 apache2-utils mysql-server memcached
-sudo apt-get -y install libapache2-mod-php php php-cli php-curl php-mcrypt php-memcache php-mysql php-mbstring
+VERSION=`cat /etc/os-release | grep VERSION_ID | awk -F '=' '{ print $2 }'`
+
+if [ "${VERSION}" = "\"14.04\"" ]
+then
+    sudo apt-get -y install apache2 apache2-utils mysql-server memcache
+    sudo apt-get -y install php5 php5-cli php5-curl php5-mcrypt php5-memcache php5-mysql
+else
+    sudo apt-get -y install apache2 apache2-utils mysql-server memcached
+    sudo apt-get -y install libapache2-mod-php php php-cli php-curl php-mcrypt php-memcache php-mysql php-mbstring
+fi
 
 for mod in proxy_wstunnel.load rewrite.load proxy.load proxy.conf proxy_http.load ssl.conf ssl.load socache_shmcb.load headers.load
 do
@@ -43,23 +51,28 @@ done
 for ctx in apache2 cli
 do
 
-#    for mod in mcrypt.ini
-#    do
-#
-#        if [ -L /etc/php/7.0/${ctx}/conf.d/${mod} ]
-#        then
-#            sudo rm /etc/7.0/php/${ctx}/conf.d/${mod}
-#        fi
-#
-#        if [ -f /etc/php/7.0/${ctx}/conf.d/${mod} ]
-#        then
-#            sudo mv /etc/php/7.0/${ctx}/conf.d/${mod} /etc/php/7.0/${ctx}/conf.d/${mod}.bak
-#        fi
-#
-#        sudo ln -s /etc/php/7.0/mods-available/${mod} /etc/php/7.0/${ctx}/conf.d/${mod}
-#    done
+    if [ "${VERSION}" = "\"14.04\"" ]
+    then
+	
+	for mod in mcrypt.ini
+	do
+	    
+	    if [ -L /etc/php5/${ctx}/conf.d/${mod} ]
+	    then
+		sudo rm /etc/php5/${ctx}/conf.d/${mod}
+	    fi
+	    
+	    if [ -f /etc/php5/${ctx}/conf.d/${mod} ]
+	    then
+		sudo mv /etc/php5/${ctx}/conf.d/${mod} /etc/php5/${ctx}/conf.d/${mod}.bak
+	    fi
+	    
+	    sudo ln -s /etc/php5/mods-available/${mod} /etc/php5/${ctx}/conf.d/${mod}
+	done
 
-    sudo perl -p -i -e "s/short_open_tag = Off/short_open_tag = On/" /etc/php/7.0/${ctx}/php.ini;
+	sudo perl -p -i -e "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/${ctx}/php.ini;
+    fi
+    
 done
 
 if [ ! -d ${ROOT}/www/templates_c ]
